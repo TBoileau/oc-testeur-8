2f11c20873f7d5db532a11a7d42b754cddd2ed2f
Thomas Boileau (toham) <t-boileau@email.com>
2023-03-02 11:35:37 +0100

    Page d'erreur (#22)
    * feat: page d'erreur (#11)

--- src/pages/Error/Error.tsx
+++ src/pages/Error/Error.tsx
@@ -1,5 +1,12 @@
+import {Link} from "react-router-dom"
+import './Error.css'
+
 export function Error(): JSX.Element {
   return (
-    <>Page d'erreur</>
+    <div className="error">
+      <h1>404</h1>
+      <p>Oups! La page que vous demandez n'existe pas.</p>
+      <Link to={"/"}>Retour sur la page d'accueil</Link>
+    </div>
   )
 }
